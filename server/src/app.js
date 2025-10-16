90a1934c0479675c6d45ed10cf97cb5a422fd0af
shreyansh0009 <saurabhsharraf@gmail.com>
2025-10-16 11:53:44 +0530

    Implement user routes and controllers; add error handling utilities and remove unused files

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,15 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(cors({
-    origin: process.env.CORS_PORT   //optional, generally front-end ip included in this.
+    origin: process.env.CORS_PORT   
 }))
 
-app.use(express.json({limit: "16kb"})) //limit is optional
+app.use(express.json({limit: "16kb"})) 
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users", userRouter)
 
 export { app }
